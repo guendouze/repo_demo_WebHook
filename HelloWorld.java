70a36f9fc85beb34eb51811583a1a6398987d7bf
GUENDOUZE Nabil <nabil.guendouze.ext@ac-grenoble.fr>
2025-03-14 10:05:51 +0100

    from my locale

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world from GitHook.");
 		System.out.println("Test 3");
+		System.out.println("Test 4444");
 	}
 }
